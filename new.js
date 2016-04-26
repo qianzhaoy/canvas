4518b3c908a7d5c75bf3459877448e8f0bfb477c
unknown <>
2016-04-26 20:38:55 +0800

    fi

---
+++ new.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.emit('some event', {
+    for: 'everyone'
+});
+io.on('connection', function (socket) {
+    socket.on('start', function (msg) {
+        io.emit('start', msg);
+    });
+});
+
+io.on('connection', function (socket) {
+    socket.on('path', function (msg) {
+        io.emit('path', msg);
+    });
+});
+io.on('connection', function (socket) {
+    socket.on('all', function (msg) {
+        io.emit('all', msg);
+    });
+});
+io.on('connection', function (socket) {
+    socket.on('clear', function (msg) {
+        io.emit('clear', msg);
+    });
+});
+app.use(express.static('static'));
+http.listen(8081, function () {
+    console.log('监听8081端口成功');
+});
